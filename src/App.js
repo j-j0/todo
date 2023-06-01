fb21a06fdc01861050bafc9c8b8623cbeb26798e
Jack <j.ackwx11@gmail.com>
2023-06-01 17:33:26 +1000

    first changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addItem = item => {
+    setList([...list, item]);
+  };
+
+  function handleSubmit(event) {
+    if (task) {
+      alert('task submitted: ' + task);
+      console.log(task);
+      addItem(task);
+      event.preventDefault();
+    } else {
+      alert('please type something before submitting');
+    }
+    
+  }
+  function handleChange(event) {
+    setTask(event.target.value);
+    console.log(event.target.value);
+  }
+  const removeItem = index => {
+    setList(list.filter((item, i) => i !== index));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <h1>Welcome!</h1>
+    <h1>To do list:</h1>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter to do list:
+        <input type="text" onChange={handleChange} name="todo" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => removeItem(index)}>Remove</button>
+          </li>
+          ))}
+      </ul>
+  </>);
+  
+    
 }
 
 export default App;
